cb452980e72f52cf36b2fdb39c5b7ac4da34bd10
mengxk <mengxk@yntrust.com>
2016-05-20 10:01:47 +0800

    ReactJs评论组件

--- demo/chapter2/build/.module-cache/fea783160181984792f18f0c32c8bd3fbb68d4eb.js
+++ demo/chapter2/build/.module-cache/fea783160181984792f18f0c32c8bd3fbb68d4eb.js
@@ -22,10 +22,8 @@ var CommentForm = React.createClass({displayName: "CommentForm",
     },
     render: function(){
         return (
-            React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}, 
-                React.createElement("input", {type: "text", placeholder: "Your name", ref: "author"}), 
-                React.createElement("input", {type: "text", placeholder: "Say something", ref: "text"}), 
-                React.createElement("input", {type: "submit", value: "Post"})
+            React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}
+                
             )
         );
     }
